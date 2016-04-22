8e0cc5c72d86029c36849dfe316bf1a1bcd06c53
Kyle Mattimore <kmattimo@umich.edu>
2016-04-21 23:43:53 -0500

    basic logo mockup. hope your browser supports vw

--- gulpfile.js
+++ gulpfile.js
@@ -44,9 +44,9 @@ gulp.task('styles', function () {
     return gulp.src(config.src.styles)
         .pipe(plugins.plumber({ errorHandler: onError }))
         .pipe(plugins.sass(config.sass.settings))
-        .pipe(plugins.cssUrlAdjuster({
-            prepend: config.dest.images
-        }))
+        // .pipe(plugins.cssUrlAdjuster({
+        //     prepend: config.dest.images
+        // }))
         .pipe(plugins.if(!config.dev, plugins.combineMediaQueries()))
         .pipe(plugins.autoprefixer(config.sass.autoprefixer))
         .pipe(plugins.if(!config.dev, plugins.csso()))
